b0bbdc974d9417eb0f694fc8c83e7f853fdda1d0
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-23 08:12:35 -0700

    add varibales

--- Class01/index.js
+++ Class01/index.js
@@ -1,4 +1,13 @@
 "use strict";
 console.log('Hellow World');
 var userName = 'Ahmed';
-console.log(userName);
+// console.log(userName)
+var passward = 123456;
+// console.log(passward)
+var num1 = 2;
+var num2 = 5;
+var num3 = '10';
+var num4 = 20;
+// SUM NUMBER AND STRING
+// console.log(num1+num3)
+console.log(num1 + num2 + num3 + num4);
--- Class01/index.ts
+++ Class01/index.ts
@@ -1,6 +1,17 @@
 console.log('Hellow World')
 
 var userName:string = 'Ahmed'
-console.log(userName)
+// console.log(userName)
 var passward:number = 123456
-console.log(passward)
+// console.log(passward)
+var num1:number = 2
+var num2:number = 5
+var num3:string= '10'
+var num4:number = 20
+
+// SUM NUMBER AND STRING
+// console.log(num1+num3)
+
+console.log(num1+num2+num3+num4)
+
+
