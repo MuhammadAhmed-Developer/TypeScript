a4769ee993fe9d37b03f32ee1cbb08409eaa6738
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-31 12:06:26 -0700

    add programs

--- Class01/index.ts
+++ Class01/index.ts
@@ -1,17 +1,33 @@
-console.log('Hellow World')
+// console.log('Hellow World')
 
-var userName:string = 'Ahmed'
-// console.log(userName)
-var passward:number = 123456
-// console.log(passward)
-var num1:number = 2
-var num2:number = 5
-var num3:string= '10'
-var num4:number = 20
+// var userName:string = 'Ahmed'
+// // console.log(userName)
+// var passward:number = 123456
+// // console.log(passward)
+// var num1:number = 2
+// var num2:number = 5
+// var num3:string= '10'
+// var num4:number = 20
+// var num5:number = 30
+// var num6:number = 30
+// var num7:number = 250
+// var num8:number = 290
+// var num9:number = 29
 
 // SUM NUMBER AND STRING
 // console.log(num1+num3)
 
-console.log(num1+num2+num3+num4)
+// console.log(num1+num2+num3+num4/num7*num8)
 
+// 1) Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
+//  var userName:string = 'Muhammad Ahmed'
+//  console.log(` “Hello ${userName}, would you like to learn some Python today?”
+//  `)
+
+// 2) Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+var getName:string = 'Muhammad ahmed' 
+console.log(getName.toLowerCase())
+console.log(getName.toUpperCase())
+console.log(getName.())
