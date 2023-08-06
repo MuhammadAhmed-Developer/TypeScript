af6dc97c7ab907068f16f6dc5927205ecec10056
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-06 04:35:15 -0700

    class02

---
+++ Class02/index.ts
@@ -0,0 +1,70 @@
+// Exercise 1:  Write a program that calculates the area of a rectangle.
+
+var width:number = 20
+var height:number= 10
+var calculate = width * height
+if(width > height){
+  console.log(`Its a Rectangle`)
+}else{
+  console.log(`Its not a Rectangle Because Height is greater then width`);
+  
+}
+
+
+// Exercise 2: Write a program that takes input and calculates the volume of a cube.
+
+var area:number = 40^3
+
+console.log(area);
+
+
+
+// Exercise 4:   Write a program that checks if a given number is even or odd.
+
+var num1:number = 30
+if(num1/2){
+  console.log(`${num1} is a Even Number`);
+  
+}else{
+  console.log(`${num1} is a Odd Number`);
+  
+}
+
+// Exercise 3:  Write a program that checks if a given number is positive, negative, or zero.
+
+var numberCheck:number= 20
+if(numberCheck>1){
+  console.log(`${numberCheck} is a positive Number` );
+  
+}
+else if(numberCheck < 0){
+  console.log(`${numberCheck} is a Negative Number` );
+  
+}
+else{
+  console.log(`${numberCheck} is equal to Zero`);
+  
+}
+
+// Exercise 5:  Write a program that determines if a person is eligible to vote based on their age.
+ 
+ var age:number = 25
+ if(age > 18){
+  console.log('You are Eligible');
+  
+ }else{
+  console.log('You are not Eligible');
+  
+ }
+
+
+// Exercise 6:  Write a program that calculates the result of a mathematical expression.	((10 + 5) * 3 - 2) / (4 % 3)  - 7)
+
+var calnum:number = ((10 + 5) * 3 - 2) / (4 % 3)  - 7
+var step1:number = (15*3-2) / 1-7
+var step2:number= 43/1-7
+var step3:number= 43-7
+var finalstep:number = 36
+console.log(calnum);
+console.log(finalstep);
+
