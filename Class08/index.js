545d7395015b39795ae0fc5c47ae3ed45aea70f2
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-03 10:13:30 +0500

    class 08

--- Class08/index.js
+++ Class08/index.js
@@ -1,17 +1,44 @@
 "use strict";
-// 1) Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
-var num1 = 10;
-var sum = 0;
-for (var i = 2; i <= num1; i += 2) {
-    sum = sum + i;
-}
-console.log(sum);
-// function sumOfFirstNEvenNumbers(n: number): number {
-//     let sum = 0;
-//     for (let i = 2; i <= n; i += 2) {
-//       sum += i;
-//     }
-//     return sum;
-//   }
-//   const n = 10;
-//   console.log(`The sum of the first ${n} even numbers is ${sumOfFirstNEvenNumbers(n)}`);
+// 1). Write a program that uses filter to remove all negative numbers from an array of numbers
+//  let array = [2, -3, 7, -9, 8, -40, 90]
+//  array = array.filter(item=>{
+//     return item < 0
+//  })
+//  console.log(array);
+//  2) // Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array where each number is multiplied by 2.
+// let array = [1, 2, 3, 4, 5]
+// array =  array.map(item=>{
+//     return item*2
+// })
+// console.log(array)
+// 3) Given an array of strings ["apple", "banana", "cherry", "date", "grape"], use the filter method to create a new array containing only the fruits with more than 5 characters.
+// let array = ["apple", "banana", "cherry", "date", "grape"]
+// array = array.filter(item=>{
+//    return item.length>5
+// })
+// console.log(array);
+// Given an array of numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], use the map and filter methods together to create a new array containing the squares of even numbers.
+//    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//    array = array.filter(item=>{
+//     return (item % 2 == 0 )
+//    }).map(item=>{
+//     return item**2
+//    })
+//    console.log(array);
+// Given an array of temperatures in Celsius [0, 10, 20, 30, 40], use the map method to create a new array where each temperature is converted to Fahrenheit using the formula (Celsius * 9/5) + 32.
+//  let array = [0, 10, 20, 30, 40]
+//  array = array.map(item=>{
+//     return (item * 9/5) + 32.
+//  })
+// console.log(array);
+// Given an array of numbers [3, 6, 9, 12, 15, 18], use the map and filter methods together to create a new array containing the doubled values of odd numbers.
+// let array = [3, 6, 9, 12, 15, 18]
+// array = array.filter(item=>{
+//     return (item%2 !== 0)
+// }).map(item=>{
+//     return item*2
+// })
+// console.log(array);
+// Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach method to log each name with an exclamation mark at the end, e.g., "Alice!".
+let array = ["Alice", "Bob", "Charlie", "David", "Emily"];
+array.forEach(item => console.log(`${item}!`));
--- Class08/index.ts
+++ Class08/index.ts
@@ -1,2 +1,67 @@
 // 1). Write a program that uses filter to remove all negative numbers from an array of numbers
 
+//  let array = [2, -3, 7, -9, 8, -40, 90]
+//  array = array.filter(item=>{
+//     return item < 0
+//  })
+//  console.log(array);
+ 
+
+
+//  2) // Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array where each number is multiplied by 2.
+
+// let array = [1, 2, 3, 4, 5]
+ 
+// array =  array.map(item=>{
+//     return item*2
+// })
+// console.log(array)
+
+
+// 3) Given an array of strings ["apple", "banana", "cherry", "date", "grape"], use the filter method to create a new array containing only the fruits with more than 5 characters.
+
+// let array = ["apple", "banana", "cherry", "date", "grape"]
+
+// array = array.filter(item=>{
+//    return item.length>5
+// })
+
+// console.log(array);
+
+
+// Given an array of numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], use the map and filter methods together to create a new array containing the squares of even numbers.
+
+//    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//    array = array.filter(item=>{
+//     return (item % 2 == 0 )
+//    }).map(item=>{
+//     return item**2
+//    })
+//    console.log(array);
+
+// Given an array of temperatures in Celsius [0, 10, 20, 30, 40], use the map method to create a new array where each temperature is converted to Fahrenheit using the formula (Celsius * 9/5) + 32.
+
+//  let array = [0, 10, 20, 30, 40]
+//  array = array.map(item=>{
+//     return (item * 9/5) + 32.
+//  })
+// console.log(array);
+
+
+// Given an array of numbers [3, 6, 9, 12, 15, 18], use the map and filter methods together to create a new array containing the doubled values of odd numbers.
+
+// let array = [3, 6, 9, 12, 15, 18]
+// array = array.filter(item=>{
+//     return (item%2 !== 0)
+// }).map(item=>{
+//     return item*2
+// })
+
+// console.log(array);
+
+// Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach method to log each name with an exclamation mark at the end, e.g., "Alice!".
+
+// let array = ["Alice", "Bob", "Charlie", "David", "Emily"]
+//    array.forEach(item=> console.log(`${item}!`))
+   
