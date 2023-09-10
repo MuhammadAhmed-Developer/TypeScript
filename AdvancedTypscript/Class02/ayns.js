552a5537badc9a40c56f9c2b195e99ab4ef328d7
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-10 13:01:12 +0500

    inquirer

--- AdvancedTypscript/Class02/ayns.js
+++ AdvancedTypscript/Class02/ayns.js
@@ -1,8 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function repeat(a) {
-    console.log(a);
-}
-setTimeout(() => {
-    repeat(45);
-}, 5000);
+import inquirer from "inquirer";
+inquirer.prompt([
+    {
+        name: "name",
+        type: "input",
+        message: "Enter Your Name?"
+    },
+    {
+        name: "age",
+        type: "number",
+        message: "Enter Your Age?"
+    },
+])
+    .then((ans) => {
+    console.log(ans);
+});
+// function repeat(a:number) {
+//      console.log(a)
+// }
+// setTimeout(() => {
+//     repeat(45)
+// }, 5000);
--- AdvancedTypscript/Class02/ayns.ts
+++ AdvancedTypscript/Class02/ayns.ts
@@ -1,9 +1,36 @@
-function repeat(a:number) {
-     console.log(a)
-}
+import inquirer from "inquirer";
 
-setTimeout(() => {
-    repeat(45)
-}, 5000);
+inquirer.prompt([
+    {
+    name:"name",
+    type:"input",
+    message:"Enter Your Name?"
+},
+    {
+    name:"age",
+    type:"number",
+    message:"Enter Your Age?"
+},
+])
+
+.then((ans)=>{
+    console.log(ans);
+})
+
+
+
+
+
+
+
+
+
+// function repeat(a:number) {
+//      console.log(a)
+// }
+
+// setTimeout(() => {
+//     repeat(45)
+// }, 5000);
 
 
